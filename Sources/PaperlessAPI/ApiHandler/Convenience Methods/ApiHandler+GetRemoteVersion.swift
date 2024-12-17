926cf2ba64dccee629729fe7814d1df5443b5312
Leo Wehrfritz <leocommander.cs@gmail.com>
2024-12-17 10:58:19 +0100

    Added static method to get remote version

--- Sources/PaperlessAPI/ApiHandler/Convenience Methods/ApiHandler+GetRemoteVersion.swift
+++ Sources/PaperlessAPI/ApiHandler/Convenience Methods/ApiHandler+GetRemoteVersion.swift
@@ -1,35 +1,27 @@
 //
-//  ApiHandler+GetAuthToken.swift
+//  ApiHandler+GetRemoteVersion.swift
 //  PaperlessAPI
 //
-//  Created by Leo Wehrfritz on 15.12.24.
+//  Created by Leo Wehrfritz on 17.12.24.
 //
 
 import Foundation
 
-public extension ApiHandler {
-    static func getAuthToken(for serverURL: URL, user: String, password: String) async throws -> String {
-        let url = serverURL.appendingPathComponent(ApiEndpoint.token.rawValue)
-        
-        let body = [
-            "username": user,
-            "password": password
-        ]
+extension ApiHandler {
+    static func getRemoteVersion(serverURL: URL) async throws -> RemoteVersion {
+        let url = serverURL.appendingPathComponent(ApiEndpoint.remoteVersion.rawValue)
         
         var request = URLRequest(url: url)
         
-        request.httpMethod = HttpMethod.post.rawValue
-        request.httpBody = try JSONEncoder().encode(body)
+        request.httpMethod = HttpMethod.get.rawValue
         request.addValue("application/json", forHTTPHeaderField: "Accept")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         
         let (data, response) = try await URLSession.shared.data(for: request)
         
         if let httpResponse = response as? HTTPURLResponse {
             if 200...299 ~= httpResponse.statusCode {
                 let decoder = JSONDecoder()
-                let token = try decoder.decode(TokenResponse.self, from: data)
-                return token.token
+                return try decoder.decode(RemoteVersion.self, from: data)
             } else if 403 == httpResponse.statusCode {
                 throw ApiError.forbidden
             } else {
